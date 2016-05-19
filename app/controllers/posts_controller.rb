e9994cb8c5e48ea2deabb442e2d8b3a1e4089982
cgungaloo <cgungaloo@yahoo.com>
2016-05-19 22:15:30 +0100

    completed run through of tutorial, more work required

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+	before_action :set_post, only: [:show, :edit, :update, :destroy]  
+
 	def index
 		@posts = Post.all
 	end
@@ -8,20 +10,46 @@ class PostsController < ApplicationController
 		@post = Post.new 
 	end
 
+	def show  
+	end 
+
+	def edit 
+
+	end
+
+	def update  
+		if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+	end  
+
+	def destroy  
+ 	 	@post.destroy
+    redirect_to root_path
+	end  
+
 	def create
-		@post =Post.create(post_params)
-		if @post.save
-			redirect_to posts_path
-		else
-			flash[:alert] = "Upload Unsuccesful"
-      		render :new
-		end
+		if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
 	end
 
 	private
 
 	def post_params
 		params.require(:post).permit(:image, :caption)
+	end 
+
+	def set_post
+		@post = Post.find(params[:id])
 	end
 
 end
